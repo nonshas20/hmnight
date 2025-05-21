39c9bb7731e2b82ee7e862e6546de3ceeb6e6fc1
HM Night Developer <user@example.com>
2025-05-21 16:14:48 +0800

    Initial commit

--- git-push.bat
+++ git-push.bat
@@ -3,7 +3,8 @@ cd c:\xampp\htdocs\hmnight
 git config --global user.email "user@example.com"
 git config --global user.name "HM Night Developer"
 git add .
-git commit -m "Improve barcode modal mobile responsiveness"
+git commit -m "Initial commit"
 git branch -M main
+git remote set-url origin https://github.com/nonshas20/hmnight.git
 git push -u origin main
 pause
